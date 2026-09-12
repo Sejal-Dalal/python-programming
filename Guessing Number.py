eedbf120b7097b53c965735675df24e744e726b3
Sejal -Dalal <sejaldalal354@gmail.com>
2026-09-12 15:43:37 +0530

    PROJECT 1: Guessing Number

---
+++ Guessing Number.py
@@ -0,0 +1,15 @@
+
+import random
+
+number = random.randint(1, 10)
+
+while True:
+    guess = int(input("Guess the number: "))
+
+    if guess == number:
+        print("Correct! 🎉")
+        break
+    elif guess > number:
+        print("Too high! Try again.")
+    else:
+        print("Too low! Try again.")
